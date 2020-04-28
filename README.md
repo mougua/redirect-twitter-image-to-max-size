7da86ce0f80f46604c7ba446df1dea6cd97058be
mougua <chengk0118@gmail.com>
2020-04-28 09:18:58 +0800

    that's all

---
+++ README.md
@@ -0,0 +1,3 @@
+#  redirect twitter image to max size
+
+这个小脚本能将你新建标签页/窗口打开的twiiter图片重定向到png格式的原图
---
+++ redirect twitter image to max size.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        redirect twitter image to max size
+// @namespace   https://github.com/mougua
+// @version     0.0.1
+// @description 将推特的图片以最大分辨率重新打开
+// @author      mougua
+// @license     MIT
+// @supportURL
+// @include     *://pbs.twimg.com/media/*
+// @exclude     *://pbs.twimg.com/media/*format=png&name=4096x4096
+// @run-at      document-start
+// @grant       none
+// ==/UserScript==
+
+window.location.replace((window.document.location + "").split('?')[0] + '?format=png&name=4096x4096');
